cb997a6a1170c7f065a9401fb33573d34a1cdf1c
LeilaC1 <leilaclesca13@gmail.com>
2023-10-24 14:26:46 -0400

    finished c to f

---
+++ Functions/c_to_f.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+
+
+
+function converCtoF(){
+ return fahrenheit;
+
+}
+let celsius = 100;
+
+let fahrenheit = (celsius * 9 / 5) + 32;
+console.log(fahrenheit);
+
+converCtoF();
+
