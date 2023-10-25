8d8e3375bde288a7c0658a64d203b2232608c6c2
LeilaC1 <leilaclesca13@gmail.com>
2023-10-25 12:26:15 -0400

    completed part code

---
+++ Strings/part_codes.js
@@ -0,0 +1,27 @@
+"use strict";
+
+
+
+
+
+
+function getSupplier (partCode){
+  let posOfColon = partCode.indexOf(":");
+  let productSupplier = partCode.substring(0, posOfColon);
+console.log(`Supplier is ${productSupplier}`);
+}
+
+function getProductNumber (partCode){
+    let posOfDash = partCode.indexOf("-");
+    let productNumber = partCode.substring( partCode.indexOf(":") + 1, posOfDash);
+    console.log(`Number is ${productNumber}`);
+}
+function getSize(partCode) {
+let size = partCode.substring(partCode.indexOf("-") + 1);
+console.log(`Size is ${size}`);
+
+}
+//
+ getSupplier("DI:12345-M" );
+getProductNumber("DI:12345-M" );
+getSize("DI:12345-M" );
