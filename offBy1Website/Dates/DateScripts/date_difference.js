871ffe1ba255151eb8249f4df3f6d6dd06c8da9c
LeilaC1 <leilaclesca13@gmail.com>
2023-10-26 10:34:41 -0400

    did date difference

---
+++ offBy1Website/Dates/DateScripts/date_difference.js
@@ -0,0 +1,13 @@
+"use strict";
+
+let startDate = "July 11, 2022";
+let endDate = "November 11, 2022";
+
+// write a code to get and display 
+
+let date1 = new Date(startDate);
+let date2 = new Date(endDate);
+
+console.log(`Start Date = ${date1}`);
+console.log(`End Date = ${date2}`);
+
