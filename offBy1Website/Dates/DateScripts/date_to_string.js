2d11a8638049d6bb1b042a63da7f0a6eb67b2ad6
LeilaC1 <leilaclesca13@gmail.com>
2023-10-26 08:55:57 -0400

    finished date to string

---
+++ offBy1Website/Dates/DateScripts/date_to_string.js
@@ -0,0 +1,14 @@
+"use strict";
+
+let currentDate = new Date();
+
+// display
+
+console.log(`toString = ${currentDate.toString()}`);
+
+console.log(`toDateString = ${currentDate.toDateString()}`);
+
+console.log(`toUTCString = ${currentDate.toUTCString()}`);
+
+console.log(`toLocaleString = ${currentDate.toLocaleString()}`)
+
