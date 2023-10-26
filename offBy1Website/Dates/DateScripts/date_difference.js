262ea67da86a3c23484abf36934e26f0cb20c848
LeilaC1 <leilaclesca13@gmail.com>
2023-10-26 10:39:08 -0400

    added day difference

--- offBy1Website/Dates/DateScripts/date_difference.js
+++ offBy1Website/Dates/DateScripts/date_difference.js
@@ -7,7 +7,11 @@ let endDate = "November 11, 2022";
 
 let date1 = new Date(startDate);
 let date2 = new Date(endDate);
+let time = Math.abs(date2 - date1);
+let days = Math.ceil(time / (1000 * 60 * 60 * 24));
 
 console.log(`Start Date = ${date1}`);
 console.log(`End Date = ${date2}`);
+console.log(`Day Difference = ${days} days`);
+
 
